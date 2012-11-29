25e59aa74dff9774cafc5a522c6895281aef6ff4
Jordan Hare <jordan.hare@gmail.com>
2012-11-29 14:54:57 -0500

    implemented directory parsing and S3 upload portion of batch runner

--- src/batch_runner.py
+++ src/batch_runner.py
@@ -1,77 +1,56 @@
+#!/usr/bin/python
+
 '''
 Created on Nov 27, 2012
 
 @author: hare
 '''
 
-
-
-'''
-read "base" files out of filesystem and store in S3 under base-key
-read "modified" files out of the filesystem and store in S3 under modify-ke
-
-spawn on-demand AWS instances
-read "base", "modified"  from S3 and generate base-graph and modify-graph, storing in S3
-spawn on-demand AWS instances
-run OTP Analyst Batch on base-graph and generate base-tiff, storing in S3
-run OTP Analyst Batch on modify-graph and generate modify-tiff, storing in S3
-'''
-
-
-
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
+import argparse
+import glob
+
+def trimSlash(dirname) :
+    if (dirname[-1:] == "/") :
+        return dirname.rstrip("/")
+    else :
+        return dirname
+
+def uploadToS3(dirname, conn) :
+    file_set = glob.glob(dirname + match_pattern)
+    bucket_name = dirname.split("/")[-1]
+    bucket = conn.create_bucket(bucket_name)
+    print "dirname " + dirname + " stored into bucket " + bucket_name 
+    for file_entry in file_set :
+        file_short = file_entry.split("/")[-1].split(".")[0]
+        key = Key(bucket)
+        key.key = file_short
+        print "storing as key: " + file_short
+        key.set_contents_from_filename(file_entry)        
+
 _AWS_ACCESS_KEY = "AKIAJ4YM7LJUTBFMED6A"
 _AWS_SECRET_KEY = "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"
+match_pattern = "/*.zip"
 
-base_path = "/otp/cache/nyc-hacked/"
-modify_path = "/otp/cache/nyc-hacked/"
+parser = argparse.ArgumentParser(description='specify two directories of GTFS. for each directory, uploaded to S3, create graph.')
+parser.add_argument('-dir1', metavar='directory_1', dest='directory_1', help='the first directory to load')
+parser.add_argument('-dir2', metavar='directory_2', dest='directory_2', help='the second directory to load')
 
+args = parser.parse_args()
+
+dir1 = trimSlash(args.directory_1)
+dir2 = trimSlash(args.directory_2)
+
+#
+# establish connection to S3
+#
 conn = S3Connection(_AWS_ACCESS_KEY, _AWS_SECRET_KEY)
-bucket = conn.create_bucket("test_bucket_otp_analyst")  
 
-bronx = Key(bucket)
-brooklyn = Key(bucket)
-manhattan = Key(bucket)
-queens = Key(bucket)
-staten = Key(bucket)
-busco = Key(bucket)
-lirr = Key(bucket)
-mnrr = Key(bucket)
-subway = Key(bucket)
-
-
-'''
-dispatch unchanged GTFS files to S3
-'''
-
-bronx.key = "bronx_base"
-bronx.set_contents_from_filename(base_path + "bronx.zip")
-
-brooklyn.key = "brooklyn_base"
-brooklyn.set_contents_from_filename(base_path + "brooklyn.zip")
-
-manhattan.key = "manhattan_base"
-manhattan.set_contents_from_filename(base_path + "manhattan.zip")
-
-queens.key = "queens_base"
-queens.set_contents_from_filename(base_path + "queens.zip")
-
-staten.key = "staten_base"
-staten.set_contents_from_filename(base_path + "staten.zip")
-
-busco.key = "busco_base"
-busco.set_contents_from_filename(base_path + "busco.zip")
-
-lirr.key = "lirr_base"
-lirr.set_contents_from_filename(base_path + "lirr.zip")
-
-mnrr.key = "mnrr_base"
-mnrr.set_contents_from_filename(base_path + "mnrr.zip")
-
-subway.key = "subway_base"
-subway.set_contents_from_filename(base_path + "subway.zip")
-
-
+#
+# upload files to S3
+#
+uploadToS3(dir1, conn)
+uploadToS3(dir2, conn)
 
